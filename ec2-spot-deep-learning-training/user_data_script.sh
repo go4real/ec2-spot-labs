ac962568799b9f3704cdd0e181adb5a40496c738
Kim <sjkim@f01898a3ba77.ant.amazon.com>
2019-07-05 02:50:35 +0900

    change region settings

--- ec2-spot-deep-learning-training/user_data_script.sh
+++ ec2-spot-deep-learning-training/user_data_script.sh
@@ -2,7 +2,7 @@
 # Get instance ID, Instance AZ, Volume ID and Volume AZ 
 INSTANCE_ID=$(curl -s http://169.254.169.254/latest/meta-data/instance-id)
 INSTANCE_AZ=$(curl -s http://169.254.169.254/latest/meta-data/placement/availability-zone)
-AWS_REGION=us-west-2
+AWS_REGION=ap-northeast-2
 
 VOLUME_ID=$(aws ec2 describe-volumes --region $AWS_REGION --filter "Name=tag:Name,Values=DL-datasets-checkpoints" --query "Volumes[].VolumeId" --output text)
 VOLUME_AZ=$(aws ec2 describe-volumes --region $AWS_REGION --filter "Name=tag:Name,Values=DL-datasets-checkpoints" --query "Volumes[].AvailabilityZone" --output text)
